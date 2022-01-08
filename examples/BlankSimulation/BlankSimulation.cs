eaba918a4c7c722f77ff846f7fd8002671c4b581
Ryan A <45476006+Redninja106@users.noreply.github.com>
2022-01-08 01:53:45 -0500

    Update BlankSimulation example to actually be a minimalist simulation. More, better examples to come!

---
+++ examples/BlankSimulation/BlankSimulation.cs
@@ -0,0 +1,20 @@
+﻿using SimulationFramework;
+using SimulationFramework.IMGUI;
+
+using var sim = new BlankSimulation();
+Simulation.RunWindowed(sim, "This is a Blank Simulation!", 1920, 1080);
+
+internal class BlankSimulation : Simulation
+{
+    public override void OnInitialize(AppConfig config)
+    {
+    }
+
+    public override void OnRender(ICanvas canvas)
+    {
+    }
+
+    public override void OnUnitialize()
+    {
+    }
+}
