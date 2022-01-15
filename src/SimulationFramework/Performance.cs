bba2e62b8417049e4b9379a21a55f4e860eb7f0d
Ryan A <45476006+Redninja106@users.noreply.github.com>
2022-01-15 18:37:06 -0500

    Add performace class for performance stats

---
+++ src/SimulationFramework/Performance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationFramework;
+
+/// <summary>
+/// Provides performance-related information.
+/// </summary>
+public static class Performance
+{
+    /// <summary>
+    /// The simulations current framerate.
+    /// </summary>
+    public static float Framerate => Simulation.Current.GetComponent<ITimeProvider>().GetFramerate();
+}
