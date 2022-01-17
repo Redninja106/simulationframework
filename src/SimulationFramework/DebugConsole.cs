72df5201141e452896dd2b802c14be7448f9d354
Ryan A <45476006+Redninja106@users.noreply.github.com>
2022-01-17 04:50:11 -0500

    implement debugConsole

--- src/SimulationFramework/DebugConsole.cs
+++ src/SimulationFramework/DebugConsole.cs
@@ -11,9 +11,15 @@ namespace SimulationFramework;
 public sealed class DebugConsole : DebugWindow
 {
     static List<string> lines = new();
+    static string input;
+    static bool justSentInput;
+    static Dictionary<string, object> commands = new();
+    static bool helpWindowOpen;
+    static bool windowJustOpened;
 
-    internal DebugConsole() : base("Debug Console")
+    internal DebugConsole() : base("Debug Console", WindowFlags.NoScrollbar | WindowFlags.NoScrollWithMouse)
     {
+        RegisterCommand("help", () => helpWindowOpen = !helpWindowOpen);
     }
 
     public static bool SilenceWarnings { get; set; }
@@ -43,7 +49,9 @@ public sealed class DebugConsole : DebugWindow
 
     protected override void OnLayout()
     {
-        if (ImGui.BeginListBox("", new Vector2(-5,-5)))
+
+
+        if (ImGui.BeginListBox("", new Vector2(-5, -25)))
         {
             foreach (var l in lines)
             {
@@ -52,5 +60,67 @@ public sealed class DebugConsole : DebugWindow
 
             ImGui.EndListBox();
         }
+
+        if (justSentInput)
+        {
+            ImGuiNET.ImGui.SetKeyboardFocusHere();
+            justSentInput = false;
+        }
+
+        if (windowJustOpened)
+        {
+            ImGuiNET.ImGui.SetKeyboardFocusHere();
+            windowJustOpened = false;
+        }
+
+        if (ImGui.InputText("", ref input, 256, InputTextFlags.EnterReturnsTrue))
+        {
+            lines.Add(">"+input);
+            var name = input.Contains(' ') ? input.Substring(0, input.IndexOf(' ')) : input;
+            if (commands.ContainsKey(name.Trim('\0').ToUpper()))
+            {
+                var command = commands[name.Trim('\0').ToUpper()];
+
+                if (command is Action noParamCmd)
+                {
+                    noParamCmd();
+                }
+                if (command is Action<string[]> paramCmd)
+                {
+                    paramCmd(input.Substring(input.IndexOf(' ')).Split(' '));
+                }
+            }
+
+            input = "";
+            justSentInput = true;
+        }
+
+        if (helpWindowOpen && ImGui.BeginWindow("Debug Console Help", ref helpWindowOpen))
+        {
+            if (ImGui.TreeNode("Available Commands:")) 
+            {
+                if (ImGui.BeginListBox("", (-5, 300)))
+                {
+                    foreach (var cmd in commands.Keys)
+                    {
+                        ImGui.Text(cmd.ToLower());
+                    }
+
+                    ImGui.EndListBox();
+                }
+                ImGui.TreePop();
+            }
+        }
+        ImGui.EndWindow();
+    }
+
+    public void RegisterCommand(string name, Action command)
+    {
+        commands.Add(name.ToUpper(), command);
+    }
+
+    public void RegisterCommand(string name, Action<string[]> command)
+    {
+        commands.Add(name.ToUpper(), command);
     }
 }
