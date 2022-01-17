e8ff80c52c96bc41597e27528dc0f5a12e94b5b5
Ryan A <45476006+Redninja106@users.noreply.github.com>
2022-01-17 13:55:52 -0500

    reimplement RunWindowed as an extension to simulation

---
+++ src/SimulationFramework.Environments.Desktop/SimulationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationFramework.Environments.Desktop;
+
+public static class SimulationExtensions
+{
+    public static void RunWindowed(this Simulation simulation, string title, int width, int height, bool resizable = true)
+    {
+        simulation.Run(new WindowEnvironment(title, width, height, resizable));
+    }
+}
