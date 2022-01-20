5825dbfa2788cfe197e1dbb3b72d9cbc75499f9f
Ryan A <45476006+Redninja106@users.noreply.github.com>
2022-01-20 09:05:11 -0500

    Updates to Rectangle

--- src/SimulationFramework/Rectangle.cs
+++ src/SimulationFramework/Rectangle.cs
@@ -31,6 +31,13 @@ public struct Rectangle : IEquatable<Rectangle>
     public float Height;
 
     /// <summary>
+    /// The size of the rectangle
+    /// </summary>
+    public Vector2 Size { get => (Width, Height); set => (Width, Height) = value; }
+
+    public Vector2 Position { get => (X, Y); set => (X, Y) = value; }
+
+    /// <summary>
     /// Creates a new rectangle from the provided position and size.
     /// </summary>
     /// <param name="position">The position of the rectangle.</param>
@@ -135,15 +142,33 @@ public struct Rectangle : IEquatable<Rectangle>
         return false;
     }
 
-    /// <summary>
-    /// </summary>
+    public static implicit operator (float, float, float, float)(Rectangle rectangle)
+    {
+        return (rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height);
+    }
+
+    public static implicit operator Rectangle((float x, float y, float width, float height) rectangle)
+    {
+        return new(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+    }
+
+    public static implicit operator (Vector2, Vector2)(Rectangle rectangle)
+    {
+        return (rectangle.Position, rectangle.Size);
+    }
+
+    public static implicit operator Rectangle((Vector2 position, Vector2 size) rectangle)
+    {
+        return new(rectangle.position, rectangle.size);
+    }
+
+    ///
     public bool Equals(Rectangle other)
     {
         return X == other.X || Y == other.Y || Width == other.Width || Height == other.Height;
     }
 
-    /// <summary>
-    /// </summary>
+    ///
     public override bool Equals([NotNullWhen(true)] object obj)
     {
         if (obj is Rectangle rect)
@@ -152,31 +177,49 @@ public struct Rectangle : IEquatable<Rectangle>
         return false;
     }
 
-    /// <summary>
-    /// </summary>
+    ///
     public override string ToString()
     {
         return $"{{{X}, {Y}, {Width}, {Height}}}";
     }
 
-    /// <summary>
-    /// </summary>
+    ///
     public static bool operator ==(Rectangle left, Rectangle right)
     {
         return left.Equals(right);
     }
     
-    /// <summary>
-    /// </summary>
+    ///
     public static bool operator !=(Rectangle left, Rectangle right)
     {
         return !(left == right);
     }
 
-    /// <summary>
-    /// </summary>
+    ///
     public override int GetHashCode()
     {
         return HashCode.Combine(X, Y, Width, Height);
+    }
+
+    /// <summary>
+    /// Creates a rectangle from the position of each side.
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    public static Rectangle CreateLTRB((float left, float top, float right, float bottom) args)
+    {
+        return CreateLTRB(args.left, args.top, args.right, args.bottom);
+    }
+
+    /// <summary>
+    /// Creates a rectangle from the position of each side.
+    /// </summary>
+    /// <param name="left">The position of the left side of the rectangle on the x-axis.</param>
+    /// <param name="top">The position of the top of the rectangle on the y-axis.</param>
+    /// <param name="right">The position of the right side of the rectangle on the x-axis.</param>
+    /// <param name="bottom">The position of the bottom of the rectangle on the y-axis.</param>
+    public static Rectangle CreateLTRB(float left, float top, float right, float bottom)
+    {
+        return new Rectangle(left, top, right - left, bottom - top);
     }
 }
