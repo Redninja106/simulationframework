f13e6c748b5870afdb3c11a7c70871e3aebb6456
Ryan A <45476006+Redninja106@users.noreply.github.com>
2022-02-16 22:23:36 -0500

    Add Gradient tests

---
+++ src/SimulationFramework.Tests.Interactive.Gradients/Program.cs
@@ -0,0 +1,27 @@
+﻿using SimulationFramework.Desktop;
+
+namespace SimulationFramework.Tests.Interactive.Gradients;
+
+public class Program : Simulation
+{
+    private static void Main()
+    {
+        using var program = new Program();
+        program.RunWindowed("Gradient Tests", 1920, 1080);
+    }
+
+    public override void OnInitialize(AppConfig config)
+    {
+    }
+
+    public override void OnRender(ICanvas canvas)
+    {
+        canvas.Translate(canvas.Width / 2, canvas.Height / 2);
+        canvas.Scale(canvas.Width / 20f);
+
+        canvas.SetDrawMode(DrawMode.Gradient);
+
+        canvas.SetGradientLinear(Alignment.TopLeft, Alignment.BottomRight, Color.Red, Color.Blue);
+        canvas.DrawRect(0,0, 1, 1, Color.Black, Alignment.Center);
+    }
+}
