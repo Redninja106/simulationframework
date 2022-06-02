53c4683505850e18c0b63ff783d7093d10e34fb2
Ryan Andersen <45476006+Redninja106@users.noreply.github.com>
2022-06-02 15:56:06 -0400

    Remove old commented-out canvas class and being to implement text

--- src/SimulationFramework.SkiaSharp/SkiaCanvas.cs
+++ src/SimulationFramework.SkiaSharp/SkiaCanvas.cs
@@ -7,224 +7,6 @@ using SkiaSharp;
 
 namespace SimulationFramework.SkiaSharp;
 
-///// <summary>
-///// Allows drawing to an <see cref="SKCanvas"/> via the <see cref="ICanvas"/> interface.
-///// </summary>
-//internal sealed class SkiaCanvas : ICanvas
-//{
-//    /// <inheritdoc/>
-//    public ITexture Target { get; }
-
-//    /// <inheritdoc/>
-//    public DrawMode DrawMode { get => currentState.DrawMode; set => currentState.UpdateDrawMode(value); }
-
-//    /// <inheritdoc/>
-//    public float StrokeWidth { get => currentState.StrokeWidth; set => currentState.UpdateStrokeWidth(value); }
-
-//    /// <inheritdoc/>
-//    public Color Color { get => currentState.FillColor; set => currentState.UpdateColor(value); }
-
-//    /// <inheritdoc/>
-//    public Matrix3x2 Transform { get => currentState.Transform; set => currentState.UpdateTransform(value); }
-
-//    /// <inheritdoc/>
-//    public FillTexture FillTexture { get => currentState.FillTexture; set => currentState.UpdateFillTexture(value); }
-
-//    /// <inheritdoc/>
-//    public Gradient Gradient { get => currentState.Gradient; set => currentState.UpdateGradient(value); }
-
-//    /// <inheritdoc/>
-//    public Rectangle? ClipRegion { get => currentState.ClipRect; set => currentState.UpdateClipRect(value); }
-
-//    // the canvas maintains a stack of state objects that can be controlled by the user
-//    private readonly Stack<SkiaCanvasState> stateStack = new();
-//    private SkiaCanvasState currentState = SkiaCanvasState.CreateDefault();
-
-//    // the graphics provider which this canvas belongs to
-//    private readonly SkiaGraphicsProvider provider;
-
-//    // internal skcanvas
-//    private readonly SKCanvas canvas;
-
-//    // do we own our skcanvas?
-//    private readonly bool owner; 
-
-//    public SkiaCanvas(SkiaGraphicsProvider provider, ITexture texture, SKCanvas canvas, bool owner)
-//    {
-//        this.Target = texture;
-//        this.provider = provider;
-//        this.canvas = canvas;
-//        this.owner = owner;
-//    }
-
-//    /// <summary>
-//    /// Returns this <see cref="SkiaCanvas"/>'s internal <see cref="SKCanvas"/>.
-//    /// </summary>
-//    internal SKCanvas GetSKCanvas()
-//    {
-//        return this.canvas;
-//    }
-
-//    public void Clear(Color color)
-//    {
-//        canvas.Clear(color.AsSKColor());
-//    }
-
-//    public void Dispose()
-//    {
-//        if (owner)
-//        {
-//            canvas.Dispose();
-//        }
-
-//        currentState.Dispose();
-
-//        while (stateStack.Any())
-//        {
-//            stateStack.Pop().Dispose();
-//        }
-//    }
-
-//    public void DrawArc(Rectangle bounds, float begin, float end, bool includeCenter)
-//    {
-//        if (bounds.Width <= 0 || bounds.Height <= 0)
-//            return;
-
-//        if (end < begin)
-//        {
-//            (begin, end) = (end, begin);
-//        }
-
-//        if (end - begin >= MathF.Tau)
-//        {
-//            canvas.DrawOval(bounds.AsSKRect(), currentState.Paint);
-//        }
-//        else 
-//        {
-//            canvas.DrawArc(bounds.AsSKRect(), begin, end - begin, includeCenter, currentState.Paint);
-//        }
-//    }
-
-//    public void DrawLine(Vector2 p1, Vector2 p2)
-//    {
-//        if (p1 == p2)
-//            return;
-
-//        canvas.DrawLine(p1.AsSKPoint(), p2.AsSKPoint(), currentState.Paint);
-//    }
-
-//    public void DrawPolygon(Span<Vector2> polygon)
-//    {
-//        using SKPath path = new();
-
-//        bool shouldClose = polygon[0] != polygon[^1];
-
-//        unsafe
-//        {
-//            fixed (Vector2* polygonPtr = polygon)
-//            {
-//                SkiaNativeApi.sk_path_add_poly(path.Handle, polygonPtr, polygon.Length, shouldClose);
-//            }
-//        }
-
-//        canvas.DrawPath(path, currentState.Paint);
-//    }
-
-//    public void DrawRoundedRect(Rectangle rect, float radius)
-//    {
-//        if (radius <= 0)
-//        {
-//            canvas.DrawRect(rect.AsSKRect(), currentState.Paint);
-//        }
-//        else
-//        {
-//            canvas.DrawRoundRect(rect.AsSKRect(), radius, radius, currentState.Paint);
-//        }
-//    }
-
-//    public void DrawText(string text, Vector2 position, Alignment alignment = Alignment.TopLeft)
-//    {
-//        throw new NotImplementedException();
-//    }
-
-//    public void DrawTexture(ITexture texture, Rectangle source, Rectangle destination)
-//    {
-//        if (texture is not SkiaTexture skiaTexture)
-//            throw new ArgumentException("texture must be a texture created by the skiasharp renderer!", nameof(texture));
-
-//        canvas.DrawBitmap(skiaTexture.GetBitmap(), source.AsSKRect(), destination.AsSKRect());
-//    }
-
-//    public Vector2 MeasureText(string text, float maxLength, out int charsMeasured)
-//    {
-//        Vector2 result;
-//        var paint = currentState.Paint;
-
-//        if (maxLength > 0)
-//        {
-//            charsMeasured = (int)paint.BreakText(text, maxLength, out result.X);
-//            result.Y = paint.TextSize;
-//        }
-//        else
-//        {
-//            charsMeasured = text.Length;
-//            result = new(paint.MeasureText(text), paint.TextSize);
-//        }
-
-//        return result;
-//    }
-
-//    public CanvasSession PushState()
-//    {
-//        currentState.Deactivate();
-
-//        stateStack.Push(currentState);
-//        currentState = currentState.Clone();
-
-//        currentState.Activate(GetSKCanvas());
-
-//        return new CanvasSession(this);
-//    }
-
-//    public void PopState()
-//    {
-//        if (!stateStack.Any())
-//            throw new InvalidOperationException();
-
-//        currentState.Deactivate();
-
-//        currentState.Dispose();
-//        currentState = stateStack.Pop();
-
-//        currentState.Activate(GetSKCanvas());
-//    }
-
-//    public void ResetState()
-//    {
-//        currentState.Deactivate();
-
-//        currentState.Dispose();
-//        currentState = SkiaCanvasState.CreateDefault();
-
-//        currentState.Activate(GetSKCanvas());
-//    }
-
-//    public bool SetFont(string fontName, TextStyles styles, float size)
-//    {
-//        throw new NotImplementedException();
-//    }
-
-//    public void Flush()
-//    {
-//        canvas.Flush();
-//    }
-
-//    public void SetFillTexture(ITexture texture, Matrix3x2 transform, TileMode tileModeX, TileMode tileModeY)
-//    {
-//        throw new NotImplementedException();
-//    }
-//}
-
 internal sealed class SkiaCanvas : ICanvas
 {
     public ITexture Target { get; }
@@ -316,12 +98,30 @@ internal sealed class SkiaCanvas : ICanvas
 
     public void DrawText(string text, Vector2 position, Alignment alignment = Alignment.TopLeft)
     {
-        throw new NotImplementedException();
+        SKRect skbounds = default;
+        currentState.Paint.MeasureText(text, ref skbounds);
+        Rectangle bounds = new(position, new(skbounds.Width, skbounds.Height), alignment);
+        canvas.DrawText(text, bounds.X - skbounds.Left, bounds.Y - skbounds.Top, currentState.Paint);
     }
 
-    public Vector2 MeasureText(string text, float maxLength, out int charsMeasured)
+    public Vector2 MeasureText(string text, float maxWidth, out int charsMeasured)
     {
-        throw new NotImplementedException();
+        if (text == null)
+        {
+            charsMeasured = 0;
+            return Vector2.Zero;
+        }
+
+        int length = text.Length;
+        if (maxWidth > 0)
+        {
+            length = (int)currentState.Paint.BreakText(text, maxWidth);
+        }
+            
+        SKRect bounds = default;
+        currentState.Paint.MeasureText(text.Length == length ? text : text[0..length], ref bounds);
+        charsMeasured = length;
+        return new(bounds.Width, bounds.Height);
     }
 
     public bool SetFont(string fontName, TextStyles styles, float size)
