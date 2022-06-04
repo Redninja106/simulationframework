caa2785597505483c2401121f50a222f31962e16
Ryan Andersen <45476006+Redninja106@users.noreply.github.com>
2022-06-04 11:22:43 -0400

    Update DrawingShapes example to use new drawing (and do a texture)

--- examples/DrawingShapes/DrawingShapes.cs
+++ examples/DrawingShapes/DrawingShapes.cs
@@ -1,4 +1,5 @@
 ﻿using SimulationFramework;
+using SimulationFramework.Drawing;
 using SimulationFramework.Drawing.Canvas;
 using System;
 using System.Numerics;
@@ -15,6 +16,16 @@ class DrawingShapesSimulation : Simulation
     {
         canvas.Clear(Color.Gray);
 
+        canvas.Fill(Color.Orange);
+        canvas.DrawPolygon(new[]
+        {
+            new Vector2(500, 10),
+            new Vector2(10, 10),
+            new Vector2(10, 500),
+            Mouse.Position,
+            new Vector2(500, 10),
+        });
+        
         canvas.Fill(Color.Red);
         canvas.DrawRect(100, 100, 100, 100);
 
@@ -29,17 +40,30 @@ class DrawingShapesSimulation : Simulation
         canvas.StrokeWidth(10);
         canvas.DrawLine(500, 500, 1000, 1000);
 
-        //canvas.StrokeWidth(0);
         canvas.DrawLine(100, 100, 500, 500);
 
-        canvas.DrawPolygon(new[]
         {
-            new Vector2(500, 10),
-            new Vector2(10, 10),
-            new Vector2(10, 500),
-            Mouse.Position,
-            new Vector2(500, 10),
-        });
+            canvas.PushState();
+            canvas.Translate(Mouse.Position);
+            canvas.DrawTexture(Graphics.LoadTexture("./logo-512x512.png"));
+            canvas.PopState();
+        }
+        
+        canvas.Font("Verdana");
+        canvas.Fill(Color.Purple);
 
+        float textX = canvas.Width / 2, textY = canvas.Height / 2;
+
+        canvas.FontStyle(72, FontStyle.Normal);
+        canvas.DrawText("Hello, World!", textX, textY, Alignment.TopRight);
+        
+        canvas.FontStyle(72, FontStyle.Bold | FontStyle.Italic);
+        canvas.DrawText("Hello, World!", textX, textY, Alignment.TopLeft);
+        
+        canvas.FontStyle(72, FontStyle.Bold | FontStyle.Strikethrough | FontStyle.Italic | FontStyle.Underline);
+        canvas.DrawText("Hello, World!", textX, textY, Alignment.BottomRight);
+        
+        canvas.FontStyle(72, FontStyle.Strikethrough | FontStyle.Underline);
+        canvas.DrawText("Hello, World!", textX, textY, Alignment.BottomLeft);
     }
 }
