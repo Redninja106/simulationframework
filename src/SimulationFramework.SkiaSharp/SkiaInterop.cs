f0510dab541a4bb91997490c87dddb96aa812a7f
Ryan Andersen <redninja106@live.com>
2023-01-10 17:17:20 -0500

    skia interop improvements

--- src/SimulationFramework.SkiaSharp/SkiaInterop.cs
+++ src/SimulationFramework.SkiaSharp/SkiaInterop.cs
@@ -13,9 +13,8 @@ public static class SkiaInterop
 {
     public static SKCanvas GetCanvas(ICanvas canvas)
     {
-        if (canvas is null)
-            return null; 
-        
+        ArgumentNullException.ThrowIfNull(canvas);
+
         if (canvas is not SkiaCanvas skiaCanvas)
             throw new ArgumentException("'canvas' must be a canvas created using the SkiaSharp graphics backend!");
 
@@ -24,8 +23,7 @@ public static class SkiaInterop
 
     public static SKBitmap GetBitmap(ITexture texture)
     {
-        if (texture is null) 
-            return null;
+        ArgumentNullException.ThrowIfNull(texture);
 
         if (texture is not SkiaTexture skiaTexture)
             throw new ArgumentException("'texture' must be a texture created using the SkiaSharp graphics backend!");
@@ -35,8 +33,7 @@ public static class SkiaInterop
 
     public static GRContext GetBackendContext(IGraphicsProvider graphics)
     {
-        if (graphics is null)
-            return null;
+        ArgumentNullException.ThrowIfNull(graphics);
         
         if (graphics is not SkiaGraphicsProvider skiaGraphics)
             throw new ArgumentException("'graphics' must be a graphics context created using the SkiaSharp graphics backend!");
@@ -49,6 +46,14 @@ public static class SkiaInterop
     {
         // there doesn't seem to be a way to do this with skiasharp...
         throw new NotSupportedException();
+    }
+
+    public static SKPaint GetPaint(CanvasState state)
+    {
+        if (state is not SkiaCanvasState skstate)
+            throw new ArgumentException("'state' must be a canvas state from a canvas created using the SkiaSharp graphics backend!");
+
+        return skstate.Paint;
     }
 
     [DllImport("Kernel32")]
