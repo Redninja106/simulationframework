f6f4d539343055ed9c89085d43f265e537d5d39e
Ryan Andersen <redninja106@live.com>
2023-04-26 17:49:23 -0400

    Add matrix builder and tests

---
+++ src/SimulationFramework/MatrixBuilder.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationFramework;
+public class MatrixBuilder
+{
+    public Matrix3x2 Matrix { get; private set; }
+    public Matrix3x2 InverseMatrix { get; private set; }
+
+    public MatrixBuilder()
+    {
+        Reset();
+    }
+
+    public MatrixBuilder Rotate(float rotation) => Rotate(rotation, Vector2.Zero);
+    public MatrixBuilder Rotate(float rotation, Vector2 centerPoint)
+    {
+        return Multiply(
+            Matrix3x2.CreateRotation(rotation, centerPoint), 
+            Matrix3x2.CreateRotation(-rotation, centerPoint)
+            );
+    }
+
+    public MatrixBuilder Translate(float x, float y) => Translate(new(x, y));
+    public MatrixBuilder Translate(Vector2 translation)
+    {
+        return Multiply(
+            Matrix3x2.CreateTranslation(translation), 
+            Matrix3x2.CreateTranslation(-translation)
+            );
+    }
+
+    public MatrixBuilder Scale(float scale) => Scale(scale, scale);
+    public MatrixBuilder Scale(float x, float y) => Scale(new Vector2(x, y));
+    public MatrixBuilder Scale(Vector2 scale) => Scale(scale, Vector2.Zero);
+    public MatrixBuilder Scale(float scale, Vector2 centerPoint) => Scale(scale, centerPoint);
+    public MatrixBuilder Scale(float x, float y, Vector2 centerPoint) => Scale(new Vector2(x, y), centerPoint);
+    public MatrixBuilder Scale(Vector2 scale, Vector2 centerPoint)
+    {
+        return Multiply(
+            Matrix3x2.CreateScale(scale, centerPoint), 
+            Matrix3x2.CreateScale(Vector2.One / scale, centerPoint)
+            );
+    }
+
+    public MatrixBuilder Skew(float radiansX, float radiansY) => Skew(radiansX, radiansY, Vector2.Zero);
+    public MatrixBuilder Skew(float radiansX, float radiansY, Vector2 centerPoint)
+    {
+        return Multiply(
+            Matrix3x2.CreateSkew(radiansX, radiansY),
+            Matrix3x2.CreateSkew(-radiansX, -radiansY)
+            );
+    }
+
+    public MatrixBuilder Multiply(Matrix3x2 matrix, Matrix3x2? inverse = null)
+    {
+        if (inverse is null)
+        {
+            if (Matrix3x2.Invert(matrix, out var invertedValue))
+            {
+                inverse = invertedValue;
+            }
+            else 
+            {
+                throw new ArgumentException("'inverse' is null and 'transform' is not invertable!");
+            } 
+        }
+
+        this.Matrix = matrix * this.Matrix;
+        this.InverseMatrix *= inverse.Value;
+
+        return this;
+    }
+
+    public MatrixBuilder Reset()
+    {
+        Matrix = Matrix3x2.Identity;
+        InverseMatrix = Matrix3x2.Identity;
+        return this;
+    }
+}
---
+++ tests/SimulationFramework.Tests/MatrixBuilderTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationFramework.Tests;
+
+[TestClass]
+public class MatrixBuilderTests
+{
+    [TestMethod]
+    public void Multiply_Uninvertable_ThrowsArgumentException()
+    {
+        var builder = new MatrixBuilder();
+
+        Assert.ThrowsException<ArgumentException>(() =>
+        {
+            builder.Multiply(default);
+        });
+    }
+
+    [TestMethod]
+    public void Multiply_NullInverted_Inverts()
+    {
+        var builder = new MatrixBuilder();
+        var expectedMatrix = Matrix3x2.CreateTranslation(10, -10) * Matrix3x2.CreateRotation(MathF.PI / 3f);
+        Matrix3x2.Invert(expectedMatrix, out var expectedInverseMatrix);
+
+        builder.Multiply(expectedMatrix);
+
+        Assert.AreEqual(expectedInverseMatrix, builder.InverseMatrix);
+    }
+
+    [TestMethod]
+    public void Multiply_Matrix_OrderOfOperations()
+    {
+        var builder = new MatrixBuilder();
+
+        // When multiplied, matrix transforms work right to left.
+        var expectedMatrix = Matrix3x2.CreateRotation(MathF.PI / 3f) * Matrix3x2.CreateTranslation(10, -10);
+
+        // With MatrixBuilder, it should work in the order which the methods called.
+        builder
+            .Multiply(Matrix3x2.CreateTranslation(10, -10))
+            .Multiply(Matrix3x2.CreateRotation(MathF.PI / 3f));
+
+        Assert.AreEqual(expectedMatrix, builder.Matrix);
+    }
+
+    [TestMethod]
+    public void Multiply_InverseMatrix_OrderOfOperations()
+    {
+        // Similar to test above, but the inverse should be in reverse order
+
+        var builder = new MatrixBuilder();
+
+        var expectedMatrix = Matrix3x2.CreateTranslation(10, -10) * Matrix3x2.CreateRotation(MathF.PI / 3f);
+
+        // Pass identity matrix for the non-inverse matrix since we don't care about it for this test
+        builder
+            .Multiply(Matrix3x2.Identity, Matrix3x2.CreateTranslation(10, -10))
+            .Multiply(Matrix3x2.Identity, Matrix3x2.CreateRotation(MathF.PI / 3f));
+
+        Assert.AreEqual(expectedMatrix, builder.InverseMatrix);
+    }
+
+    [TestMethod]
+    public void Multiply_RoundTrip()
+    {
+        // Tests if the InverseMatrix property is actually the inverse of the Matrix property after Multiply is called.
+
+        var builder = new MatrixBuilder()
+            .Multiply(Matrix3x2.CreateTranslation(10, -10))
+            .Multiply(Matrix3x2.CreateRotation(MathF.PI / 3f));
+
+        Assert.AreEqual(Matrix3x2.Identity, builder.Matrix * builder.InverseMatrix);
+    }
+
+    [TestMethod]
+    public void Reset_SetsIdentity()
+    {
+        var builder = new MatrixBuilder()
+            .Multiply(Matrix3x2.Identity, Matrix3x2.CreateTranslation(10, -10))
+            .Multiply(Matrix3x2.Identity, Matrix3x2.CreateRotation(MathF.PI / 3f));
+
+
+        builder.Reset();
+
+        Assert.AreEqual(Matrix3x2.Identity, builder.Matrix);
+        Assert.AreEqual(Matrix3x2.Identity, builder.InverseMatrix);
+
+    }
+}
