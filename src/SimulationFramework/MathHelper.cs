c6dcb2047eb8c453cfcd50014c60ca04bc7317c0
Ryan Andersen <redninja106@live.com>
2023-06-29 21:38:07 -0400

    Remove redundant method

--- src/SimulationFramework/MathHelper.cs
+++ src/SimulationFramework/MathHelper.cs
@@ -10,37 +10,6 @@ namespace SimulationFramework;
 public static class MathHelper
 {
     /// <summary>
-    /// Finds the bounding box of a polygon.
-    /// </summary>
-    /// <param name="polygon">The polygon to find the bounds of.</param>
-    public static unsafe Rectangle PolygonBounds(Span<Vector2> polygon)
-    {
-        fixed (Vector2* polygonPtr = &polygon[0])
-        {
-            return PolygonBounds(CollectionsHelper.AsEnumerableUnsafe(polygonPtr, polygon.Length));
-        }
-    }
-
-    /// <summary>
-    /// Finds the bounding box of a polygon.
-    /// </summary>
-    /// <param name="polygon">The polygon to find the bounds of.</param>
-    public static Rectangle PolygonBounds(IEnumerable<Vector2> polygon)
-    {
-        float minX = float.PositiveInfinity, minY = float.PositiveInfinity, maxX = float.NegativeInfinity, maxY = float.NegativeInfinity;
-
-        foreach (var point in polygon)
-        {
-            maxX = MathF.Max(maxX, point.X);
-            maxY = MathF.Max(maxY, point.Y);
-            minX = MathF.Min(minX, point.X);
-            minY = MathF.Min(minY, point.Y);
-        }
-
-        return new Rectangle(minX, minY, maxX - minX, maxY - minY);
-    }
-
-    /// <summary>
     /// Linearly interpolates between two values.
     /// </summary>
     /// <param name="a">The first value.</param>
