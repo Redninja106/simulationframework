491503fa54bcbe0efeb96735af6633c458834367
Ryan Andersen <redninja106@live.com>
2023-09-04 14:25:50 -0400

    restore blankSimulation example

--- examples/BlankSimulation/Program.cs
+++ examples/BlankSimulation/Program.cs
@@ -6,17 +6,12 @@ Start<Program>();
 
 partial class Program : Simulation
 {
-    ITexture tex;
     public override void OnInitialize()
     {
-        tex = Graphics.CreateTexture(1024, 1024);
-        tex.GetCanvas().Clear(Color.Red);
-        Window.SetIcon(tex);
     }
 
     public override void OnRender(ICanvas canvas)
     {
         canvas.Clear(Color.Black);
-        canvas.DrawTexture(tex);
     }
 }
