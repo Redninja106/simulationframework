a8f287837142addddf7a66d5a204ab1188204f27
Ryan Andersen <redninja106@live.com>
2023-10-21 16:48:29 -0400

    remove test from example

--- examples/BlankSimulation/Program.cs
+++ examples/BlankSimulation/Program.cs
@@ -12,13 +12,5 @@ partial class Program : Simulation
 
     public override void OnRender(ICanvas canvas)
     {
-        canvas.Clear(Color.Black);
-        canvas.DrawText(Performance.Framerate.ToString(), 0, 0);
-        Performance.FramerateAverageDuration = 5;
-
-        if (Keyboard.IsKeyPressed(Key.Space))
-        {
-            Graphics.SwapInterval = 1&~Graphics.SwapInterval;
-        }
     }
 }
