be24ea08cb2a0c3308e8aa61aa669b9e76478ed9
Ryan Andersen <45476006+Redninja106@users.noreply.github.com>
2024-02-29 14:43:38 -0500

    make blank example actually blank

--- examples/BlankSimulation/Program.cs
+++ examples/BlankSimulation/Program.cs
@@ -7,18 +7,12 @@ Start<Program>(new DesktopPlatform());
 
 partial class Program : Simulation
 {
-    private ITexture texture;
-
     public override void OnInitialize()
     {
-        texture = Graphics.CreateTexture(256, 256);
-        texture.GetCanvas().Clear(Color.Black);
     }
 
     public override void OnRender(ICanvas canvas)
     {
         canvas.Clear(Color.White);
-        canvas.Fill(texture);
-        canvas.DrawRoundedRect(new Vector2(canvas.Width / 2, canvas.Height / 2), Vector2.One * 256, 10, Alignment.Center);
     }
 }
