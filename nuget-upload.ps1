d1dd69af82cbe7f5f3212a02c7789dd3d8db87b7
Ryan Andersen <45476006+Redninja106@users.noreply.github.com>
2024-11-19 00:57:22 -0500

    alpha 11

--- nuget-upload.ps1
+++ nuget-upload.ps1
@@ -1,4 +1,5 @@
 $nuget_api_key = cat "NUGET_API_KEY"
+if (-not $?) { Exit }
 
 $version=Read-Host("version to upload?")
 
@@ -9,11 +10,11 @@ if ($(Read-Host("correct? (y/n)")) -ne "y") {
 
 dotnet clean
 if (-not $?) { Exit }
-dotnet build
+dotnet pack
 if (-not $?) { Exit }
 dotnet test
 if (-not $?) { Exit }
 
 dotnet nuget push $("src/SimulationFramework/bin/Release/SimulationFramework." + $version + ".nupkg") --api-key $nuget_api_key --source https://api.nuget.org/v3/index.json
 dotnet nuget push $("src/SimulationFramework.OpenGL/bin/Release/SimulationFramework.OpenGL." + $version + ".nupkg") --api-key $nuget_api_key --source https://api.nuget.org/v3/index.json
-dotnet nuget push $("src/SimulationFramework.Desktop/bin/Release/SimulationFramework.Desktop" + $version + ".nupkg") --api-key $nuget_api_key --source https://api.nuget.org/v3/index.json
+dotnet nuget push $("src/SimulationFramework.Desktop/bin/Release/SimulationFramework.Desktop." + $version + ".nupkg") --api-key $nuget_api_key --source https://api.nuget.org/v3/index.json
